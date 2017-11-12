61691dfc139561998a4423c5f015cb3c8a2db762
Kevin Hou <kevin.ch.hou@gmail.com>
2017-11-12 03:44:48 -0500

    Updated test site, styling fixes

--- readme.md
+++ readme.md
@@ -1,3 +1,41 @@
-# HTMLexa
+<img src="https://github.com/khou22/HTMLexa/raw/master/cover.png" />
 
-<img src="./cover.png" />
+## Introduction
+*HTMLexa* is an Alexa Skill with a companion [web app](https://htmlexa.herokuapp.com/test) that allows people to build full-scale websites with his or her voice. We aimed to improve the accessibility of website creation through machine learning and a well-crafted user-experience spanning both the voice and digital spectrums.
+
+## Inspiration
+With the rise of technology and the resoluting Digital Divide, the Internet has become the underpinnings of modern communication. *Access* to information as well as *publishing* information is crutial to the success of a business, movement, or personal brand. We wanted to improve the accessibility of website creation by utilizing the power of Amazon Alexa.
+
+With *HTMLexa*, the barrier for entry in creating websites has been drastically reduced. Voice commands provide ease of use to an entire new segment of the population including children, the blind, those without technological experience, the elderly, and many more. Website building — even drag and drop sites like Squarespace and Wix — are too complicated and time consuming for a vast majority of people. *HTMLexa* provides a painless way for people to creates online content — thereby instantly reaching millions of new people.
+
+## What It Does
+*HTMLexa* is an Alexa Skill that allows a user to dictate the creation of website. You can add titles, paragraphs, buttons, and even images to create a functional, powerful website with a modern look and feel. Here are a few examples:
+
+1. "*Add a big image of of Princeton University*"
+2. "*Move paragraph-1 into the left column below title-1*"
+3. "*Add a banner with a beach background and the title, 'Welcome'*"
+
+
+## How We Built It
+In order to achieve the maximum level of continuity and flexiblity for different types of websites, we set out to create a design system of sorts. We started by creating mockups of common websites and picking out the most freqently used. Next, we decided which properties on each component would be user contributed. Finally, we created a collection of common elements that we would implement using ReactJS.
+
+The Amazon Alexa functionality was built using Amazon's Alexa SDK. The scripting was done primary in AWS Lambda. We implemented our own database on AWS S3 because we didn't want to add the complexities of a PostGres or relational database.
+
+The client app actually refers to both a frontend client and a server. Both the client and server are build using Webpack and served up using ExpressJS. The frontend is a React/Redux/Router stack with an ES6 and ExpressJS backend. The persist data is stored using Amazon S3 to allow for seamless data continuity across Lambda and the client.
+
+## Challenges
+We faced many challenges, in building this product. One of the major hurdles we faced was simply the engineering of code that writes other code. We essentially coded an app that would use machine learning write completely new code. This concept was at first difficult to design, but we soon were able to come up with a logical solution to this problem in the form of a dynamically compiled website.
+
+We also encountered a handful of issues connecting various systems together. With everything from Amazon Alexa, Lambda, and S3 to Heroku, Python, and Webpack, our stack covered a wide variety of programming languages, systems, and designs. Getting the puzzle pieces to all fit together was a challenging, yet very rewarding problem to solve.
+
+## Accomplishments That We're Proud Of
+We are proud of the fact that we were able to tackle an immensly difficult challenge and come out with a polished, powerful, working product. While it is not perfect, given the 36 hour time period, we were able to accomplish quite a great deal. One of them ost interesting and rewarding experiences was building a user experience that spanned both voice and digital. It was an engaging adventure that provoked a lot of creativity and ingenuity.
+
+## What We Learned
+We learned an incredible amount about Amazon's Alexa as well as the design of large-scale systems. We also learned the principles of a unified component library and the benefits of building modular code.
+
+## What's Next for HTMLexa
+We are hoping to improve the client-side admin functionalities so that you can edit the site to a higher granularity using a web app platform. We're also hoping to add new components and features like multiple pages, templates, etc.
+
+## Built With
+Alexa Skills, AWS Lambda, Python, AWS S3, Heroku, NodeJS, React/Redux/Router, ExpressJS, and Webpack. The designs were done with Sketch.
--- server/src/data/sampleSchema.js
+++ server/src/data/sampleSchema.js
@@ -2,7 +2,7 @@
 // Hard coded for testing purposes
 export default {
     defaults: {
-        title: 'My Website',
+        title: 'Shop Acme - Fall 2017',
         defaultColors: {
             primary: '#5092DF',
             lighter: '#90B9FD',
@@ -13,26 +13,25 @@ export default {
         {
             type: 'banner',
             id: 'banner-1',
-            content: 'Hello, World.',
+            content: 'FALL 2017',
             column: 0,
             description: {
-                textColor: 'default',
+                textColor: 'white',
                 size: 0,
                 backgroundColor: 'white',
                 font: {
-                    weight: 0,
+                    weight: 1,
                     oblique: false,
                     underline: false,
                 },
 
-                // Specific to Button
-                link: 'https://www.princeton.edu/sites/default/files/images/2017/06/20060425_NassauHall_JJ_IMG_5973.jpg',
+                link: 'https://orig00.deviantart.net/bc57/f/2015/134/8/8/dark_autumn_by_nelleke-d8tbosc.png',
             },
         },
         {
             type: 'title',
             id: 'title-1',
-            content: 'I am a title',
+            content: 'Shop our latest collection for Fall 2017',
             column: 0,
             description: {
                 textColor: 'default',
@@ -46,9 +45,25 @@ export default {
             },
         },
         {
-            type: 'subtitle',
-            id: 'subtitle-1',
-            content: 'I am a subtitle',
+            type: 'image',
+            id: 'image-3',
+            content: 'https://i.pinimg.com/originals/27/e0/3d/27e03d0713f447c6a150660e1516354c.jpg',
+            column: 0,
+            description: {
+                textColor: 'default',
+                size: 1,
+                backgroundColor: 'white',
+                font: {
+                    weight: 0,
+                    oblique: false,
+                    underline: false,
+                },
+            },
+        },
+        {
+            type: 'title',
+            id: 'title-2',
+            content: 'About',
             column: 0,
             description: {
                 textColor: 'default',
@@ -64,7 +79,74 @@ export default {
         {
             type: 'paragraph',
             id: 'paragraph-1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere id magna sit amet sodales. Aenean in elit vel nisi luctus placerat. Curabitur non consequat massa. Donec turpis eros, molestie quis egestas sodales, pharetra sed massa. Etiam facilisis ac nisl eget imperdiet. Curabitur tristique aliquet nulla, blandit lacinia massa pulvinar eget. Praesent nunc lorem, tincidunt eget quam eu, consectetur interdum sem. Aenean mollis ultrices neque. Mauris interdum vel arcu at molestie. Fusce rhoncus ut nibh eu congue. Pellentesque cursus dolor est. Fusce pulvinar nulla ut ante mattis auctor. Fusce pretium tortor diam, eu tincidunt tortor interdum vel. Ut fringilla eu nunc a consequat. Ut facilisis imperdiet fermentum.',
+            content: 'Our new collection features work from popular minimalist designer Jill Sanders.',
+            column: 0,
+            description: {
+                textColor: 'default',
+                size: 0,
+                backgroundColor: 'white',
+                font: {
+                    weight: 0,
+                    oblique: false,
+                    underline: false,
+                },
+            },
+        },
+        {
+            type: 'button',
+            id: 'button-1',
+            content: 'More',
+            column: 0,
+            description: {
+                textColor: 'default',
+                size: 0,
+                backgroundColor: 'white',
+                font: {
+                    weight: 0,
+                    oblique: false,
+                    underline: false,
+                },
+
+                // Specific to Button
+                link: 'www.amazon.com',
+            },
+        },
+        {
+            type: 'image',
+            id: 'image-2',
+            content: 'http://andesigns.us/wp-content/uploads/2016/08/minimal-white-chair.jpg',
+            column: 0,
+            description: {
+                textColor: 'default',
+                size: 0,
+                backgroundColor: 'white',
+                font: {
+                    weight: 0,
+                    oblique: false,
+                    underline: false,
+                },
+            },
+        },
+        {
+            type: 'title',
+            id: 'title-3',
+            content: 'Product',
+            column: 0,
+            description: {
+                textColor: 'default',
+                size: 0,
+                backgroundColor: 'white',
+                font: {
+                    weight: 0,
+                    oblique: false,
+                    underline: false,
+                },
+            },
+        },
+        {
+            type: 'subtitle',
+            id: 'subtitle-2',
+            content: 'COLLABORATION WITH JILL SANDERS',
             column: 0,
             description: {
                 textColor: 'default',
@@ -80,87 +162,7 @@ export default {
         {
             type: 'paragraph',
             id: 'paragraph-2',
-            content: 'Lorem ipsum dolor sit amet. (Heavy font)',
-            column: 1,
-            description: {
-                textColor: '#3286A8',
-                size: 0,
-                backgroundColor: '#EEE',
-                font: {
-                    weight: 1,
-                    oblique: false,
-                    underline: false,
-                },
-            },
-        },
-        {
-            type: 'paragraph',
-            id: 'paragraph-3',
-            content: 'Lorem ipsum dolor sit amet.',
-            column: 2,
-            description: {
-                textColor: 'default',
-                size: 0,
-                backgroundColor: 'white',
-                font: {
-                    weight: -1,
-                    oblique: true,
-                    underline: true,
-                },
-            },
-        },
-        {
-            type: 'image',
-            id: 'image-1',
-            content: 'https://www.princeton.edu/sites/default/files/images/2017/06/20060425_NassauHall_JJ_IMG_5973.jpg',
-            column: 1,
-            description: {
-                textColor: 'default',
-                size: -1,
-                backgroundColor: 'white',
-                font: {
-                    weight: 0,
-                    oblique: false,
-                    underline: false,
-                },
-            },
-        },
-        {
-            type: 'image',
-            id: 'image-2',
-            content: 'https://www.princeton.edu/sites/default/files/images/2017/06/20060425_NassauHall_JJ_IMG_5973.jpg',
-            column: 2,
-            description: {
-                textColor: 'default',
-                size: 0,
-                backgroundColor: 'white',
-                font: {
-                    weight: 0,
-                    oblique: false,
-                    underline: false,
-                },
-            },
-        },
-        {
-            type: 'image',
-            id: 'image-3',
-            content: 'https://www.princeton.edu/sites/default/files/images/2017/06/20060425_NassauHall_JJ_IMG_5973.jpg',
-            column: 0,
-            description: {
-                textColor: 'default',
-                size: 1,
-                backgroundColor: 'white',
-                font: {
-                    weight: 0,
-                    oblique: false,
-                    underline: false,
-                },
-            },
-        },
-        {
-            type: 'button',
-            id: 'button-1',
-            content: 'Button',
+            content: 'This season’s collection focuses on the art of minimal furniture design. Our collection features work curated to show our users the best designs.',
             column: 0,
             description: {
                 textColor: 'default',
@@ -171,9 +173,6 @@ export default {
                     oblique: false,
                     underline: false,
                 },
-
-                // Specific to Button
-                link: 'https://www.princeton.edu/sites/default/files/images/2017/06/20060425_NassauHall_JJ_IMG_5973.jpg',
             },
         },
     ],
